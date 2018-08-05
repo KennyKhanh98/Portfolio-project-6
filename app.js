865bc4d9e614b345aad9c54b2610ee32a9ccc299
Ken Le <leankhanh98@gmail.com>
2018-08-04 21:51:41 -0400

    add event listener for reset play

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const overlay = document.getElementById('overlay');
 const startButton = document.querySelector('.btn__reset');
 const keyrowButton = document.getElementsByClassName('.keyrow button');
 const ulList = document.querySelector('#phrase ul');
-const listPhrase = document.getElementById('list');
+// const list = phrase.querySelector('ul');
+const listElement = phrase.querySelectorAll('li');
 const letterClass = document.getElementsByClassName('letter');
 const showClass = document.getElementsByClassName('show');
 const title = document.querySelector('.title');
+const hearts = document.querySelectorAll('.tries');
 
-var missed = 0;
+let missed = 0;
 
 const phrases = ['i love you',
                   'life is short',
@@ -17,21 +19,14 @@ const phrases = ['i love you',
                   'my sunshine',
                   'every moment matters'];
 
-// let phraseArray = getRandomPhraseAsArray(phrases);
-
 startButton.addEventListener('click', () => {
   overlay.style.display = 'none';
-  let phraseArray = getRandomPhraseAsArray(phrases);
-  addPhraseToDisplay(phraseArray);
 });
-
-function randomArr(arr) {
-    return Math.floor(Math.random() * arr.length);
-}
 
 function getRandomPhraseAsArray(arr){
     //do stuff to any arr that is passed in
-    const ranPhrase = phrases[randomArr(arr)];
+    const random = Math.floor(Math.random() * arr.length);
+    const ranPhrase = phrases[random];
     const char = ranPhrase.split('');
     return char;
 }
@@ -40,16 +35,19 @@ function addPhraseToDisplay(arr){
     // do stuff any arr that is passed in, and add to `#phrase ul`
     for (let i = 0; i < arr.length; i++) {
       const char = arr[i];
-      const list = document.createElement('li');
-      list.textContent = char;
-      ulList.appendChild(list);
-      if (list.textContent !== " ") {
-        list.classList.add("letter");
+      const newList = document.createElement('li');
+      newList.textContent = char;
+      ulList.appendChild(newList);
+      if (newList.textContent !== " ") {
+        newList.classList.add("letter");
       } else {
-        list.classList.add("space");
+        newList.classList.add("space");
       }
     }
 }
+
+let phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
 
 function checkLetter(button) {
   let found = null;
@@ -66,13 +64,11 @@ function checkWin() {
   if (showClass.length === letterClass.length) {
     overlay.style.display = 'flex';
     overlay.classList.add("win");
-    // overlay.setAttribute('class', 'win', 'start');
     title.textContent = 'Congratulations on winning!';
     startButton.textContent = 'Play again';
   } else if (missed >= 5) {
     overlay.style.display = 'flex';
     overlay.classList.add("lose");
-    // overlay.setAttribute('class', 'lose', 'start');
     title.textContent = 'Better luck next time';
     startButton.textContent = 'Play again';
   }
@@ -84,8 +80,37 @@ qwerty.addEventListener('click', (e) => {
     e.target.setAttribute("disabled", true);
     const letterFound = checkLetter(e.target);
     if (letterFound === null) {
+      let addMissed = missed;
+      hearts[addMissed].setAttribute('src', 'images/lostHeart.png');
       missed += 1;
     }
   }
   checkWin();
+});
+
+startButton.addEventListener('click', (e) => {
+  if (e.target.textContent === 'Play again') {
+    overlay.style.display = 'none';
+    missed = 0;
+
+    // Reset hearts
+    for (let i = 0; i < hearts.length; i++) {
+      hearts[i].setAttribute('src', 'images/liveHeart.png');
+    }
+
+    // Delete last guessed word
+    for (let i = 0; i < listElement.length; i++) {
+      ulList.removeChild(listElement);
+    }
+
+    // Reset button's attributes
+    for (let i = 0; i < keyrowButton.length; i++) {
+      keyrowButton[i].classList.remove("chosen");
+      keyrowButton[i].setAttribute("disabled", false);
+    }
+
+    // Add new phrase
+    phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
+  }
 });
